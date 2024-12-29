76dc10e1d40e765a025130e463f5887b8c50721c
张伟峰 <wulakaka@live.com>
2024-12-29 19:46:43 +0800

    feat: 副作用函数改为注册形式

--- src/effect.ts
+++ src/effect.ts
@@ -1,26 +1,31 @@
-const bucket = new Set<() => void>()
+let activeEffect: () => void;
 
-const data = {text: 'hello world'}
-
-const obj = new Proxy(data, {
-    get(target: { text: string }, key: string | symbol) {
-        bucket.add(effect)
-        // 为啥会出现 Reflect
-        return target[key as keyof typeof target]
-    },
-    set(target: { text: string }, key: string | symbol, newVal: string) {
-        target[key as keyof typeof target] = newVal
-        bucket.forEach(fn => fn())
-        return true
-    }
-})
-
-function effect() {
-    document.body.innerText = obj.text
+function effect(fn: () => void) {
+  activeEffect = fn;
+  fn();
 }
 
-effect()
+const bucket = new Set<() => void>();
+
+const data = { text: "hello world" };
+
+const obj = new Proxy(data, {
+  get(target: { text: string }, key: string | symbol) {
+    activeEffect && bucket.add(activeEffect);
+    // 为啥会出现 Reflect
+    return target[key as keyof typeof target];
+  },
+  set(target: { text: string }, key: string | symbol, newVal: string) {
+    target[key as keyof typeof target] = newVal;
+    bucket.forEach((fn) => fn());
+    return true;
+  },
+});
+
+effect(() => {
+  document.body.innerText = obj.text;
+});
 
 setTimeout(() => {
-    obj.text = 'hello vue3'
-}, 1000)
+  obj.text = "hello vue3";
+}, 1000);
