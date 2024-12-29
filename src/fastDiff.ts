b496c5c505491dc5008243b8fd90095af18c09a6
张伟峰 <wulakaka@live.com>
2024-12-29 23:00:16 +0800

    feat(快速diff): 预处理

---
+++ src/fastDiff.ts
@@ -0,0 +1,46 @@
+function patchKeyedChildren(n1, n2, container) {
+  const newChildren = n2.children;
+  const oldChildren = n1.children;
+
+  let j = 0;
+  let oldVNode = oldChildren[j];
+  let newVNode = newChildren[j];
+  // while 循环向后遍历，直到遇到不同 key 的节点为止
+  while (oldVNode.key === newVNode.key) {
+    patch(oldVNode, newVNode, container);
+    j++;
+    oldVNode = oldChildren[j];
+    newVNode = newChildren[j];
+  }
+
+  let oldEnd = oldChildren.length - 1;
+  let newEnd = newChildren.length - 1;
+  oldVNode = oldChildren[oldEnd];
+  newVNode = newChildren[newEnd];
+  // while 循环向前遍历，直到遇到不同 key 的节点为止
+  while (oldVNode.key === newVNode.key) {
+    patch(oldVNode, newVNode, container);
+    oldEnd--;
+    newEnd--;
+    oldVNode = oldChildren[oldEnd];
+    newVNode = newChildren[newEnd];
+  }
+
+  // 预处理完成后，如果满足以下条件，则说明从 j --> newEnd 的节点应作为新节点插入
+  if (j > oldEnd && j <= newEnd) {
+    // 锚点索引
+    const anchorIndex = newEnd + 1;
+    // 锚点元素
+    const anchor =
+      anchorIndex < newChildren.length ? newChildren[anchorIndex].el : null;
+    // 采用 while 循环，调用 patch 函数逐个挂载新增节点
+    while (j <= newEnd) {
+      patch(null, newChildren[j++], container, anchor);
+    }
+  } else if (j > newEnd && j <= oldEnd) {
+    // j --> oldEnd 的节点应该被移除
+    while (j <= oldEnd) {
+      unmount(oldChildren[j++]);
+    }
+  }
+}
