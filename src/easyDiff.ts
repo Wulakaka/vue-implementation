90ef6206e1cc7d9eed7ca62b2623a4ef47623720
张伟峰 <wulakaka@live.com>
2024-12-29 21:43:43 +0800

    feat(简单diff): 找到需要移动的元素

--- src/easyDiff.ts
+++ src/easyDiff.ts
@@ -3,34 +3,61 @@
 interface VNode {
   type: string;
   children: string | VNode[];
+  key?: string | number;
 }
 
 const oldVnode: VNode = {
   type: "div",
   children: [
-    { type: "p", children: "1" },
-    { type: "p", children: "2" },
-    { type: "p", children: "3" },
+    { type: "p", children: "1", key: 1 },
+    { type: "p", children: "2", key: 2 },
+    { type: "p", children: "3", key: 3 },
   ],
 };
 
 const newVnode: VNode = {
   type: "div",
   children: [
-    { type: "p", children: "4" },
-    { type: "p", children: "5" },
-    { type: "p", children: "6" },
+    { type: "p", children: "3", key: 3 },
+    { type: "p", children: "1", key: 1 },
+    { type: "p", children: "2", key: 2 },
   ],
 };
 
 function patchChildren(n1: VNode, n2: VNode, container: HTMLElement) {
-  if (typeof n1.children === "string") {
+  if (typeof n2.children === "string") {
     // 省略
   } else if (Array.isArray(n2.children)) {
     // 重新实现两组子节点的更新方式
     // 新旧 children
     const oldChildren = n1.children;
     const newChildren = n2.children;
+
+    // 用来存储寻找过程中遇到的最大索引值
+    let lastIndex = 0;
+    // 遍历新的 children
+    for (let i = 0; i < newChildren.length; i++) {
+      const newVNode = newChildren[i];
+      // 遍历旧的 children
+      for (let j = 0; j < oldChildren.length; j++) {
+        const oldVNode = oldChildren[j];
+        // 如果找到了相同 key 的节点，说明可以复用，但仍然需要调用 patch 函数更新
+        if (newVNode.key === oldVNode.key) {
+          // 调用 patch 函数更新该节点
+          patch(oldVNode, newVNode, container);
+          if (j < lastIndex) {
+            // 如果当前找到的节点在旧 children 中的索引值小于最大索引值 lastIndex
+            // 说明该节点对应的真实 DOM 需要移动
+          } else {
+            // 如果当前找到的节点在旧 children 中的索引不小于最大索引值
+            // 更新最大索引值
+            lastIndex = j;
+          }
+          break;
+        }
+      }
+    }
+
     const oldLength = oldChildren.length;
     const newLength = newChildren.length;
     // 获取两组 children 的较小长度
